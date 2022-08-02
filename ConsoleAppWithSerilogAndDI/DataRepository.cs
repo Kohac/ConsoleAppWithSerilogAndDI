139be57e1f09de88867196e17325c9c2dbcda2c7
kohy44@seznam.cz <kohy44@seznam.cz>
2022-08-02 22:22:34 +0200

    Add project files.

---
+++ ConsoleAppWithSerilogAndDI/DataRepository.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppWithSerilogAndDI
+{
+    public class DataRepository : IDataRepository
+    {
+        private readonly ILogger<DataRepository> _log;
+        private readonly IConfiguration _config;
+
+        public DataRepository(ILogger<DataRepository> log, IConfiguration config)
+        {
+            _log = log;
+            _config = config;
+        }
+
+        public void Connect()
+        {
+            _log.LogInformation($"Connection string {_config.GetConnectionString("DefaultConnection")}");
+        }
+    }
+}
---
+++ ConsoleAppWithSerilogAndDI/Program.cs
@@ -0,0 +1,38 @@
+﻿using ConsoleAppWithSerilogAndDI;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+Console.WriteLine("Hello, World!");
+var host = AppStartup();
+var dataService = ActivatorUtilities.CreateInstance<DataRepository>(host.Services);
+dataService.Connect();
+    
+static void ConfigSetup(IConfigurationBuilder builder)
+{
+    builder.SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile(Path.Combine(AppDomain.CurrentDomain.BaseDirectory,"appsettings.json"), optional: false, reloadOnChange: true)
+        .AddEnvironmentVariables();
+}
+
+static IHost AppStartup()
+{
+    var builder = new ConfigurationBuilder();
+    ConfigSetup(builder);
+
+    Log.Logger = new LoggerConfiguration()
+        .ReadFrom.Configuration(builder.Build())
+        .Enrich.FromLogContext()
+        .WriteTo.Console()
+        .CreateLogger();
+
+    var host = Host.CreateDefaultBuilder()
+                    .ConfigureServices((context, services) =>
+                    {
+                        services.AddScoped<IDataRepository, DataRepository>();
+                    })
+                    .UseSerilog()
+                    .Build();
+    return host;
+}
